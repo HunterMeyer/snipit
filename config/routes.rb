02341c1a629b7e389c75b37c730f9ee17d817fdd
HunterMeyer <huntermeyer@live.com>
2015-03-14 00:22:34 -0400

    Add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 SnipIt::Application.routes.draw do
   root 'home#index'
 
+  get 'home/index'
+  
+  post 'api/signup'
+  post 'api/signin'
+  post 'api/reset_password'
+
+  # post 'api/upload_snippet'
+  # get 'api/get_snippet'
+    
+  get 'api/get_token'  
+  get 'api/clear_token'
 end
