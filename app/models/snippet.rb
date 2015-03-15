566f28fda15658494b391b191c0cc0b5509de82e
HunterMeyer <huntermeyer@live.com>
2015-03-15 02:53:01 -0400

    Add snippet model

---
+++ app/models/snippet.rb
@@ -0,0 +1,9 @@
+class Snippet < ActiveRecord::Base
+  belongs_to :user
+
+  def to_json(options={})
+    options[:except] ||= [:id, :user_id, :created_at, :updated_at]
+    super(options)
+  end
+  
+end
