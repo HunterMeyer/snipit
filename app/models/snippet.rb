b7394b915988af2625fda38c2615ca2c4b0e0cfe
HunterMeyer <huntermeyer@live.com>
2015-03-15 02:58:38 -0400

    Set Snippet random reference on intialize

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,10 @@
 class Snippet < ActiveRecord::Base
   belongs_to :user
+  after_initialize :generate_reference
+
+  def generate_reference
+    self.reference ||= "#{user_id}_#{DateTime.now.to_i}_#{SecureRandom.hex(6)}"
+  end
 
   def to_json(options={})
     options[:except] ||= [:id, :user_id, :created_at, :updated_at]
