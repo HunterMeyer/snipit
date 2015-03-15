040c1a5f28c0f99988529bace63dd79f6d1c6565
HunterMeyer <huntermeyer@live.com>
2015-03-15 03:04:05 -0400

    Open upload/get Snippet routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ SnipIt::Application.routes.draw do
   post 'api/signin'
   post 'api/reset_password'
 
-  # post 'api/upload_snippet'
-  # get 'api/get_snippet'
+  post 'api/upload_snippet'
+  get 'api/get_snippet'
     
   get 'api/get_token'  
   get 'api/clear_token'
