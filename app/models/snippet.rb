2ca3415dee0494559b933b13b54671ca5387aee0
HunterMeyer <huntermeyer@live.com>
2015-03-15 03:31:49 -0400

    Set from on Snippet create

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,9 +1,14 @@
 class Snippet < ActiveRecord::Base
   belongs_to :user
   after_initialize :generate_reference
+  after_create     :set_from
 
   def generate_reference
     self.reference ||= "#{user_id}_#{DateTime.now.to_i}_#{SecureRandom.hex(6)}"
+  end
+
+  def set_from
+    self.update(from: "#{self.user.first_name} #{self.user.last_name}")
   end
 
   def to_json(options={})
